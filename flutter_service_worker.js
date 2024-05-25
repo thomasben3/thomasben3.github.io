0fffce593cc92be43eb11718cd40588eb31ca572
Thomas Bensemhoun <thomasbensemhoun1@gmail.com>
2024-05-26 01:49:23 +0200

    ajustement valeurs inertie encore + mtn les barres chargent en meme temps que le slider qui disparait

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "55376483d19133475d8a0f8adca0bb14",
+const RESOURCES = {"flutter_bootstrap.js": "ebe7634982aba1b2244087e5021e80b5",
 "version.json": "397d7b1db12086c749ae59a64846ebe6",
 "index.html": "c52924d6505c0477ea17e969cd41e8d9",
 "/": "c52924d6505c0477ea17e969cd41e8d9",
-"main.dart.js": "60b517d71a3ff9f5c8100e1f19ac828c",
+"main.dart.js": "8290dcacb503c0f01d8dc9b608f582ce",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
