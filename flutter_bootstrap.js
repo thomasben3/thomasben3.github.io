49827ece80eabd676b77c129589b91237663b5e2
Thomas Bensemhoun <thomasbensemhoun1@gmail.com>
2024-05-28 14:55:56 +0200

    premier vrai test avec page complète

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "581017684"
+    serviceWorkerVersion: "2043697419"
   }
 });
